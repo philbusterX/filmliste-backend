b4c509b6cd85860795e6da2f7b90cf476dabb964
philbusterX <kumaigorodskii@gmail.com>
2025-05-26 00:05:57 +0200

    fix: Update CORS to allow deployed frontend origin

--- src/main/java/filmliste_backend/controller/MovieController.java
+++ src/main/java/filmliste_backend/controller/MovieController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "https://filmliste-frontend.onrender.com"})
 public class MovieController {
 
     @GetMapping("/movies")
